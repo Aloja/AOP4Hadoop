30f54b2ab735684afd4928a11bf59a49d2fa4216
alejandromontero <alejandro.montero.rivero@gmail.com>
2015-08-31 15:41:13 +0200

    .gitignore works now

--- insert_hooks.sh
+++ insert_hooks.sh
@@ -1,8 +1,69 @@
 #!/bin/bash 
 
+echo "###########################################################"
+echo "################# SETTING UP VARIABLES ####################"
+echo "###########################################################"
+
+. user-provided-env.sh
 . local_environment.sh
 
 mkdir -p $RELEASE_PATH
+mkdir -p $INSTRUMENTATION_PATH
 
 #REPLACE WITH AJC CODE
-cp $HADOOP_CORE_FILE_PATH $PATCHED_HADOOP_CORE_FILE
+
+echo "###########################################################"
+echo "################# WEAVING NEW HADOOP CORE #################"
+echo "###########################################################"
+
+echo ""
+
+echo "###########################################################"
+echo "################# SETTING CLASSPATH #######################"
+echo "###########################################################"
+
+for i in `ls ${HADOOP_PREFIX}/lib/*.jar `
+do
+	export CLASSPATH=$CLASSPATH:"${i}"
+done
+
+for i in `ls ${HADOOP_PREFIX}/lib/jsp-2.1/*.jar `
+do
+	export CLASSPATH=$CLASSPATH:"${i}"
+done
+
+
+echo "###########################################################"
+echo "################# WEAVING HADOOP CORE #####################"
+echo "###########################################################"
+
+echo "date,event moment,event,PID,Hostname" >> $INSTRUMENTATION_PATH/log.csv
+
+ajc -1.5 -classpath ${CLASSPATH} -inpath ${HADOOP_CORE_FILE_PATH} $SOURCE_AJC/Aspectj.aj -outjar $PATCHED_HADOOP_CORE_FILE >> $INSTRUMENTATION_PATH/detectedPointCuts.txt
+
+
+cp $HADOOP_CORE_FILE_PATH $PATCHED_HADOOP_CORE_FILE 
+
+
+echo "###########################################################"
+echo "################# DONE ####################################"
+echo "###########################################################"
+
+
+echo "###########################################################"
+echo "############### STARTING HADOOP CLUSTER ###################"
+echo "###########################################################"
+
+$HADOOP_PREFIX/bin/start-all.sh
+
+echo "###########################################################"
+echo "###################### EXECUTING JOB ######################"
+echo "###########################################################"
+
+$JOBS_PREFIX/terasort.sh
+
+echo "###########################################################"
+echo "############### STOPPING HADOOP CLUSTER ###################"
+echo "###########################################################"
+
+$HADOOP_PREFIX/bin/stop-all.sh
---
+++ jobs/sort.sh
@@ -0,0 +1,26 @@
+HADOOP_EXECUTABLE="$HADOOP_PREFIX/bin/hadoop"
+HADOOP_EXAMPLES_JAR="$HADOOP_PREFIX/hadoop-examples-1.0.3.jar"
+
+DATA_HDFS=/HiBench
+INPUT_HDFS=${DATA_HDFS}/Sort/Input
+OUTPUT_HDFS=${DATA_HDFS}/Sort/Output
+
+# sort 400MB total
+# for prepare (per node) - 200MB/node
+DATASIZE=200000000
+NUM_MAPS=2
+# for running (in total)
+NUM_REDS=4
+
+$HADOOP_EXECUTABLE jar $HADOOP_EXAMPLES_JAR randomtextwriter \
+-D test.randomtextwrite.bytes_per_map=$((${DATASIZE} / ${NUM_MAPS})) \
+-D test.randomtextwrite.maps_per_host=${NUM_MAPS} \
+$INPUT_HDFS
+
+sleep 5
+
+$HADOOP_EXECUTABLE jar $HADOOP_EXAMPLES_JAR sort \
+-outKey org.apache.hadoop.io.Text \
+-outValue org.apache.hadoop.io.Text \
+-r ${NUM_REDS} \
+$INPUT_HDFS $OUTPUT_HDFS
