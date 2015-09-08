1e6f7877f2b426c3e39cf8eea9abeafbb74d5c62
alejandromontero <alejandro.montero.rivero@gmail.com>
2015-09-08 18:50:37 +0200

    Added mapper and reducer instrumentation

---
+++ example_job.sh
@@ -0,0 +1,29 @@
+#!/bin/bash 
+
+. local_environment.sh
+. set_tracing_environment.sh
+
+echo "###########################################################"
+echo "############### STARTING HADOOP CLUSTER ###################"
+echo "###########################################################"
+
+$HADOOP_PREFIX/bin/stop-all.sh
+rm -rf /tmp/hadoop/*
+rm -rf $HADOOP_PREFIX/logs/*
+
+$HADOOP_PREFIX/bin/hadoop namenode -format
+$HADOOP_PREFIX/bin/start-all.sh
+
+echo "###########################################################"
+echo "###################### EXECUTING JOB ######################"
+echo "###########################################################"
+
+
+
+$JOBS_PREFIX/terasort.sh
+
+echo "###########################################################"
+echo "############### STOPPING HADOOP CLUSTER ###################"
+echo "###########################################################"
+
+$HADOOP_PREFIX/bin/stop-all.sh
--- insert_hooks.sh
+++ insert_hooks.sh
@@ -4,11 +4,12 @@ echo "###########################################################"
 echo "################# SETTING UP VARIABLES ####################"
 echo "###########################################################"
 
-. user-provided-env.sh
 . local_environment.sh
 
 mkdir -p $RELEASE_PATH
 mkdir -p $INSTRUMENTATION_PATH
+echo $INSTRUMENTATION_PATH > /dev/shm/last_execution
+echo `pwd` > /dev/shm/pwd_path
 
 #REPLACE WITH AJC CODE
 
@@ -37,34 +38,11 @@ echo "###########################################################"
 echo "################# WEAVING HADOOP CORE #####################"
 echo "###########################################################"
 
-echo "date,event moment,event,PID,Hostname" >> $INSTRUMENTATION_PATH/log.csv
+echo "date,event moment,event,PID,Hostname,Extra data" > $INSTRUMENTATION_PATH/log.csv
 
-ajc -1.5 -classpath ${CLASSPATH} -inpath ${HADOOP_CORE_FILE_PATH} $SOURCE_AJC/Aspectj.aj -outjar $PATCHED_HADOOP_CORE_FILE >> $INSTRUMENTATION_PATH/detectedPointCuts.txt
-
-
-cp $HADOOP_CORE_FILE_PATH $PATCHED_HADOOP_CORE_FILE 
+ajc -1.5 -showWeaveInfo -classpath ${CLASSPATH} -inpath ${HADOOP_CORE_FILE_PATH} $SOURCE_AJC/Aspectj.aj -outjar $PATCHED_HADOOP_CORE_FILE > $INSTRUMENTATION_PATH/detectedPointCuts.txt
 
 
 echo "###########################################################"
 echo "################# DONE ####################################"
 echo "###########################################################"
-
-
-echo "###########################################################"
-echo "############### STARTING HADOOP CLUSTER ###################"
-echo "###########################################################"
-
-$HADOOP_PREFIX/bin/hadoop namenode -format
-$HADOOP_PREFIX/bin/start-all.sh
-
-echo "###########################################################"
-echo "###################### EXECUTING JOB ######################"
-echo "###########################################################"
-
-$JOBS_PREFIX/terasort.sh
-
-echo "###########################################################"
-echo "############### STOPPING HADOOP CLUSTER ###################"
-echo "###########################################################"
-
-$HADOOP_PREFIX/bin/stop-all.sh
