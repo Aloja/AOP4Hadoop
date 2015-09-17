0c7ab13ea9c5cf9731648892fc926f321e900300
alejandromontero <alejandro.montero.rivero@gmail.com>
2015-09-17 18:18:34 +0200

    Hadoop 2.6 first commit

--- insert_hooks.sh
+++ insert_hooks.sh
@@ -23,26 +23,37 @@ echo "###########################################################"
 echo "################# SETTING CLASSPATH #######################"
 echo "###########################################################"
 
-for i in `ls ${HADOOP_PREFIX}/lib/*.jar `
+for i in `ls ${AOP_PREFIX}/lib/*.jar `
 do
 	export CLASSPATH=$CLASSPATH:"${i}"
 done
 
-for i in `ls ${HADOOP_PREFIX}/lib/jsp-2.1/*.jar `
+for i in `ls ${HADOOP_PREFIX}/share/hadoop/common/lib/*.jar `
 do
 	export CLASSPATH=$CLASSPATH:"${i}"
 done
 
+for i in `ls ${JAR_PREFIX}/common/*.jar `
+do
+	export CLASSPATH=$CLASSPATH:"${i}"
+done
 
 echo "###########################################################"
-echo "################# WEAVING HADOOP CORE #####################"
+echo "################# WEAVING HADOOP  #########################"
 echo "###########################################################"
 
-echo "date,event moment,event,PID,Hostname,Extra data" > $INSTRUMENTATION_PATH/log.csv
+#echo "date,event moment,event,PID,Hostname,Extra data" > $INSTRUMENTATION_PATH/log.csv
 
-ajc -1.5 -showWeaveInfo -classpath ${CLASSPATH} -inpath ${HADOOP_CORE_FILE_PATH} $SOURCE_AJC/Aspectj.aj -outjar $PATCHED_HADOOP_CORE_FILE > $INSTRUMENTATION_PATH/detectedPointCuts.txt
 
+echo "################# WEAVING CORE ##########################"
+
+
+ajc -showWeaveInfo -classpath ${CLASSPATH} -inpath ${HADOOP_CORE_FILE_PATH} $SOURCE_AJC/Aspect-2.0.6.aj -outjar $PATCHED_HADOOP_CORE_FILE > $INSTRUMENTATION_PATH/detectedPointCuts.txt
+
+echo "################# WEAVING MAPRED ##########################"
+
+ajc -showWeaveInfo -classpath ${CLASSPATH} -inpath ${HADOOP_MAPRED_FILE_PATH} $SOURCE_AJC/Aspect-2.0.6.aj -outjar $PATCHED_HADOOP_MAPRED_FILE > $INSTRUMENTATION_PATH/detectedPointCuts.txt
 
 echo "###########################################################"
-echo "################# DONE ####################################"
+echo "################# WEAVING COMPLETE ########################"
 echo "###########################################################"
--- user-provided-env.sh
+++ user-provided-env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 
 
-export HADOOP_PREFIX=/home/vagrant/workspace/src/hadoop-1.0.3
-export HADOOP_VERSION=1.0.3
+export HADOOP_PREFIX=/home/vagrant/workspace/src/hadoop-2.6.0
+export HADOOP_VERSION=2.6.0
