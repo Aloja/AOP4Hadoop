1b794dcca13a27e052320960e497a469c476ae3d
alejandromontero <alejandro.montero.rivero@gmail.com>
2015-09-25 13:02:13 +0200

    fix merge issues

--- insert_hooks.sh
+++ insert_hooks.sh
@@ -28,14 +28,6 @@ do
 	export CLASSPATH=$CLASSPATH:"${i}"
 done
 
-<<<<<<< HEAD
-for i in `ls ${HADOOP_PREFIX}/share/hadoop/hdfs/lib/*.jar `
-do
-	export CLASSPATH=$CLASSPATH:"${i}"
-done
-
-=======
->>>>>>> 0c7ab13ea9c5cf9731648892fc926f321e900300
 for i in `ls ${HADOOP_PREFIX}/share/hadoop/common/lib/*.jar `
 do
 	export CLASSPATH=$CLASSPATH:"${i}"
@@ -46,11 +38,7 @@ do
 	export CLASSPATH=$CLASSPATH:"${i}"
 done
 
-<<<<<<< HEAD
-export CLASSPATH=$CLASSPATH:${HADOOP_PREFIX}/share/hadoop/yarn/hadoop-yarn-common-2.6.0.jar
 
-=======
->>>>>>> 0c7ab13ea9c5cf9731648892fc926f321e900300
 echo "###########################################################"
 echo "################# WEAVING HADOOP  #########################"
 echo "###########################################################"
@@ -58,7 +46,6 @@ echo "###########################################################"
 #echo "date,event moment,event,PID,Hostname,Extra data" > $INSTRUMENTATION_PATH/log.csv
 
 
-<<<<<<< HEAD
 # echo "################# WEAVING CORE ##########################"
 
 
@@ -71,16 +58,6 @@ echo "################# WEAVING MAPRED ##########################"
 echo "################# WEAVING HDFS ##########################"
 
 ajc -1.5 -showWeaveInfo -classpath ${CLASSPATH} -inpath ${HADOOP_HDFS_FILE_PATH} $SOURCE_AJC/Aspect-2.6.0-hdfs.aj -outjar $PATCHED_HADOOP_HDFS_FILE >> $INSTRUMENTATION_PATH/detectedPointCuts.txt
-=======
-echo "################# WEAVING CORE ##########################"
-
-
-ajc -showWeaveInfo -classpath ${CLASSPATH} -inpath ${HADOOP_CORE_FILE_PATH} $SOURCE_AJC/Aspect-2.0.6.aj -outjar $PATCHED_HADOOP_CORE_FILE > $INSTRUMENTATION_PATH/detectedPointCuts.txt
-
-echo "################# WEAVING MAPRED ##########################"
-
-ajc -showWeaveInfo -classpath ${CLASSPATH} -inpath ${HADOOP_MAPRED_FILE_PATH} $SOURCE_AJC/Aspect-2.0.6.aj -outjar $PATCHED_HADOOP_MAPRED_FILE > $INSTRUMENTATION_PATH/detectedPointCuts.txt
->>>>>>> 0c7ab13ea9c5cf9731648892fc926f321e900300
 
 echo "###########################################################"
 echo "################# WEAVING COMPLETE ########################"
--- jobs/terasort.sh
+++ jobs/terasort.sh
@@ -1,10 +1,5 @@
 HADOOP_EXECUTABLE="$HADOOP_PREFIX/bin/hadoop"
-<<<<<<< HEAD
 HADOOP_EXAMPLES_JAR="$HADOOP_PREFIX/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.6.0.jar"
-=======
-HADOOP_EXAMPLES_JAR="$HADOOP_PREFIX/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.6.0.jar
-"
->>>>>>> 0c7ab13ea9c5cf9731648892fc926f321e900300
 
 DATA_HDFS=/HiBench
 INPUT_HDFS=${DATA_HDFS}/Terasort/Input
@@ -15,11 +10,8 @@ CONFIG_REDUCER_NUMBER=mapred.reduce.tasks
 
 # for prepare (total) - 200MB
 # DATASIZE specifies the number of blocks of 100 bytes each
-<<<<<<< HEAD
-DATASIZE=200000000
-=======
+                  
 DATASIZE=2000000
->>>>>>> 0c7ab13ea9c5cf9731648892fc926f321e900300
 NUM_MAPS=4
 # for running (total)
 NUM_REDS=4
